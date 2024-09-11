cdf10da896b9607e2a885d1ef67fff7096785b19
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-09-11 09:25:31 +0500

    Create Q3.py

---
+++ Lab/04/Q3.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def Print_biodata(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+student1 = Student("Syed Ukkashah", 20)
+student1.Print_biodata()
