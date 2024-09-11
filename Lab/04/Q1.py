8ddd035c815780777829612871ca9cc4fc211cd1
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-09-11 08:49:16 +0500

    Update Q1.py

--- Lab/04/Q1.py
+++ Lab/04/Q1.py
@@ -2,3 +2,4 @@ a = ["He", "th", "i", "sal"]
 b = ["llo", "is", "s", "man"]
 ab = [i + j for i, j in zip(a, b)]
 print(ab)  
+print([x + y for x, y in zip(["He", "th", "i", "sal"], ["llo", "is", "s", "man"])]) #written in one line
