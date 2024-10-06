c22119aaa8fb7c71047e6fc7ec55b7ef988e6fe1
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-10-06 23:32:56 +0500

    Update numpy_notes.py

--- Notes/numpy_notes.py
+++ Notes/numpy_notes.py
@@ -4,11 +4,11 @@ my_array = np.array(my_list)
 print("Original list:", my_list)
 print("Converted NumPy array:", my_array)
 print(np.arange(2, 17)) # return evenly spaced values from 2-16
-print(np.arange(0,11,2)) # returns evenly spaced values from 0-10 with intervals of 2
+print(np.arange(0,11,2)) # returns evenly spaced values from 0-10 with intervals of 2 (0,2,4,6,8,10)
 print(np.zeros(5)) #returns array of 3 0s
 print(np.zeros((5,3))) #2D array
 print(np.ones(4))
-print(np.linspace(0,10,3)) #array of evenly spaced nums 0-10 with 3 intervals (0,5,10)
+print(np.linspace(0,10,3)) #array of 3 evenly spaced nums 0-10 (0,5,10)
 print(np.eye(4)) # 4x4 identity matrix
 print(np.random.rand(2)) #2 rand nums
 print(np.random.rand(5,5)) #5X5 rand nums
